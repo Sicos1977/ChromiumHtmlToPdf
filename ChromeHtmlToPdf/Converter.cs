c25f3405292f94a4f4904042687f65bce98146ba
sicos1977 <sicos2002@hotmail.com>
2017-07-15 18:34:24 +0200

    Start of new project

---
+++ ChromeHtmlToPdf/Converter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromeHtmlToPdf
+{
+    public class Converter
+    {
+    }
+}
+/*
+                 '--headless'=>'',
+                '--disable-gpu'=>'',
+                '--incognito'=>'',
+                '--enable-viewport'=>'',
+ */
