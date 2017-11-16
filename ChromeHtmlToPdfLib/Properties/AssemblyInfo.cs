db37e12fd7afb385c79e65d79274b0fc1574c424
Kees <magic-sessions@home.nl>
2017-11-16 08:50:34 +0100

    Renamed lib file and added console project

--- ChromeHtmlToPdfLib/Properties/AssemblyInfo.cs
+++ ChromeHtmlToPdfLib/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChromeHtmlToPdf")]
+[assembly: AssemblyTitle("ChromeHtmlToPdfLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
-[assembly: AssemblyProduct("ChromeHtmlToPdf")]
-[assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2017")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChromeHtmlToPdfLib")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6b9bbcf3-bae4-47cb-bf46-b616c734947d")]
+[assembly: Guid("0e189418-89f1-4842-8329-033949d8aa6f")]
 
 // Version information for an assembly consists of the following four values:
 //
