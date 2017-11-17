ae256db1bd2d8e516fa6d952a53ae3e90cb41f8f
sicos1977 <sicos2002@hotmail.com>
2017-11-17 13:43:06 +0100

    Added copyright information

--- ChromeHtmlToPdf/Properties/AssemblyInfo.cs
+++ ChromeHtmlToPdf/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ChromeHtmlToPdf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyCompany("Magic-Sessions.com - Kees van Spelde")]
 [assembly: AssemblyProduct("ChromeHtmlToPdf")]
-[assembly: AssemblyCopyright("Copyright ChromeHtmlToPdf 2017")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
