883feaddf02cf487785a93d69605053af82956ab
sicos1977 <sicos2002@hotmail.com>
2019-01-07 13:19:23 +0100

    Added dotnetstandard version

---
+++ ChromeHtmlToPdfLibStandard/Exceptions/ConversionException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ChromeHtmlToPdfLib.Exceptions
+{
+    /// <summary>
+    /// Raised when the PDF conversion fails
+    /// </summary>
+    [Serializable]
+    internal class ConversionException : Exception
+    {
+        protected ConversionException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        internal ConversionException() { }
+
+        internal ConversionException(string message) : base(message) { }
+
+        internal ConversionException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
