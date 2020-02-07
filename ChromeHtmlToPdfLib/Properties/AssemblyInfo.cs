72df2a375757989fcaa4d7a8a85c10f2b2e565e2
Kees van Spelde <keesvanspelde@hotmail.com>
2020-02-07 15:22:32 +0100

    Upped version number

--- ChromeHtmlToPdfLib/Properties/AssemblyInfo.cs
+++ ChromeHtmlToPdfLib/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Magic-Sessions.com - Kees van Spelde")]
 [assembly: AssemblyProduct("ChromeHtmlToPdfLib")]
-[assembly: AssemblyCopyright("Copyright © 2017 - 2019 - Kees van Spelde")]
+[assembly: AssemblyCopyright("Copyright © 2017-2020 - Kees van Spelde")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.5.1.0")]
+[assembly: AssemblyFileVersion("1.5.1.0")]
