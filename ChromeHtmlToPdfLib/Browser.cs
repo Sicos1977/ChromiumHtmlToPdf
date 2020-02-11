857876ec1de7f8ee39f70d72acd45ce6818bd1b4
Kees <magic-sessions@home.nl>
2020-02-11 13:08:32 +0100

    Remove unused using

--- ChromeHtmlToPdfLib/Browser.cs
+++ ChromeHtmlToPdfLib/Browser.cs
@@ -27,7 +27,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
