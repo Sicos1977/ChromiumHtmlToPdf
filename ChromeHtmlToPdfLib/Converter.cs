1841d81e7ca18d934ed4eccea4688fc251ef4d5d
Kees <magic-sessions@home.nl>
2020-04-09 09:55:10 +0200

    Silence reSharper

--- ChromeHtmlToPdfLib/Converter.cs
+++ ChromeHtmlToPdfLib/Converter.cs
@@ -39,6 +39,8 @@ using System.Threading;
 using ChromeHtmlToPdfLib.Enums;
 using ChromeHtmlToPdfLib.Exceptions;
 using ChromeHtmlToPdfLib.Helpers;
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable MemberCanBePrivate.Global
 
 namespace ChromeHtmlToPdfLib
 {
