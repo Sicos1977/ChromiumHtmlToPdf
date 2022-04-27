3392506eb4bd64179cb13fd5662f7aa93cff8c85
Kees van Spelde <keesvanspelde@hotmail.com>
2022-04-27 15:27:28 +0200

    Also kill the child processes

--- ChromeHtmlToPdfLib/Converter.cs
+++ ChromeHtmlToPdfLib/Converter.cs
@@ -1766,7 +1766,7 @@ namespace ChromeHtmlToPdfLib
             try
             {
                 var process = Process.GetProcessById(processId);
-                process.Kill();
+                process.Kill(true);
             }
             catch (Exception exception)
             {
